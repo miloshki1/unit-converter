c934f57ca198c6472f9bacc93cee159f003cb05a
miloshki1 <124580227+miloshki1@users.noreply.github.com>
2023-02-13 14:49:45 +0000

    Add files via upload

---
+++ unit converts (temperature, currency, and measurements.py
@@ -0,0 +1,18 @@
+celsius = float(input("Enter temperature in celsius: ")) #celsius into farenheight
+farenheight = celsius * 9/5 + 32 
+print(farenheight)
+
+
+euros = float(input("how many euros do you wanna convert:  ")) #euros into dollars
+dollars = euros * 0.0547 + euros
+print(dollars)
+
+
+kilometers = float(input("insert the amount of kilometers you want to convert into miles:  ")) #kilometers into miles 
+miles = kilometers * 0.62137
+print(miles)
+
+
+pounds = float(input("input the amount of pounds you want converted into kilograms")) #pounds into kilograms
+kilograms = pounds * 0.453592
+print(kilograms)
